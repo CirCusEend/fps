b84842ac320baea771ee2f7b8fa631c8803190d4
CirCusEend <39886350+CirCusEend@users.noreply.github.com>
2018-12-28 17:09:48 +0100

    jump toegevoegd

--- FPS/Assets/Scripts/Jump.cs
+++ FPS/Assets/Scripts/Jump.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Rigidbody rb;
+    public float jumppower;
+
+    // Use this for initialization
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumppower, ForceMode.Impulse);
+        }
+    }
 }
