947e49013ee59bed808e35e78615834c43c138b7
jochemlam <skyherojochempie@gmail.com>
2018-12-28 18:22:57 +0100

    updated mousecontrols

--- FPS/Assets/Scripts/MouseControls.cs
+++ FPS/Assets/Scripts/MouseControls.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MouseControls : MonoBehaviour {
 
+    private bool isLocked;
+
     private void MakeCursorLocked()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        isLocked = true;
     }
 
     private void MakeCursorUnlocked()
     {
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        isLocked = false;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.P) && isLocked == false)
+        {
+            MakeCursorLocked();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && isLocked == true)
+        {
+            MakeCursorUnlocked();
+        }
     }
 }
